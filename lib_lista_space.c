36aa9f10794c31ddbab5aaa8611b2f5fa8a40509
Leonardo Krambeck <lk19@inf.ufpr.br>
2019-11-20 21:57:06 -0300

    finalmente encarrilhou

---
+++ lib_lista_space.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib_lista_space.h"
+
+int inicializa_lista(t_lista *l)
+{
+	t_nodo *sentinel;
+
+	sentinel = (t_nodo*) malloc (sizeof (t_nodo));
+	if (sentinel == NULL)
+		return 0;
+
+	sentinel->prox = NULL;
+	sentinel->prev = NULL;
+
+	l->ini = sentinel;
+	
+	sentinel = (t_nodo*) malloc (sizeof (t_nodo));
+	if (sentinel == NULL)
+		return 0;
+
+	sentinel->prox = NULL;
+	sentinel->prev = l->ini;
+
+	l->ini->prox = sentinel;
+	l->fim = sentinel;
+
+	l->atual = NULL;
+	l->tamanho = 0;
+
+	return 1;
+}
+
+int lista_vazia(t_lista *l)
+{
+	if (l->tamanho == 0)
+		return 1;
+
+	return 0;
+}
+
+void destroi_lista(t_lista *l)
+{
+	if (l->ini == NULL && l->atual == NULL && l->fim == NULL && l->tamanho == 0)
+		return;
+
+	t_nodo *p;
+
+	while (l->ini != NULL)
+	{
+		p = l->ini;
+		l->ini = p->prox;
+		free (p);
+	}
+
+	l->atual = NULL;
+	l->fim = NULL;
+	l->tamanho = 0;
+}
+
+int tamanho_lista(int *tam, t_lista *l)
+{
+	if (l->ini == NULL && l->fim == NULL)
+		return 0;
+
+	*tam = l->tamanho;
+	return 1;
+}
+
+int insere_fim_lista(int pos_x, int pos_y, int tam_x, int tam_y, int status, int speed, t_lista *l)
+{
+	t_nodo *new;
+	new = (t_nodo*) malloc (sizeof (t_nodo));
+	if (new == NULL)
+		return 0;
+
+	new->pos_x = pos_x;
+	new->pos_y = pos_y;
+	new->tam_x = tam_x;
+	new->tam_y = tam_y;
+	new->status = status;
+	new->speed = speed;
+	new->prox = l->fim;
+	new->prev = l->fim->prev;
+
+	l->fim->prev->prox = new;
+	l->fim->prev = new;
+
+	l->tamanho++;
+
+	return 1;		
+}
+
+int inicializa_atual_inicio(t_lista *l)
+{
+	if (lista_vazia(l))
+		return 0;
+
+	l->atual = l->ini->prox;
+	return 1;
+}
+
+void incrementa_atual(t_lista *l)
+{
+	if (lista_vazia(l) || l->atual == NULL) 
+		return;
+
+	if (l->atual == l->fim)
+		return;
+
+	l->atual = l->atual->prox;
+}
+
+int consulta_item_atual(int *pos_x, int *pos_y, int *tam_x, int *tam_y, int *status, int *speed, t_lista *atual)
+{
+	if (atual->atual == NULL) 
+		return 0;
+
+	if (atual->atual == atual->ini || atual->atual == atual->fim)
+		return 0;
+
+	*pos_x = atual->atual->pos_x;
+	*pos_y = atual->atual->pos_y;
+	*tam_x = atual->atual->tam_x;
+	*tam_y = atual->atual->tam_y;
+	*status = atual->atual->status;
+	*speed = atual->atual->speed;
+	return 1;
+}
+
+int remove_item_atual(int *pos_x, int *pos_y, int *tam_x, int *tam_y, int *status, int *speed, t_lista *l)
+{
+	if (l->atual == NULL)
+		return 0;
+
+	if (l->atual == l->ini || l->atual == l->fim)
+		return 0;
+
+	t_nodo *p = l->atual;
+	p->prev->prox = p->prox;
+	p->prox->prev = p->prev;
+
+	l->atual = p->prox;
+
+	*pos_x = p->pos_x;
+	*pos_y = p->pos_y;
+	*tam_x = p->tam_x;
+	*tam_y = p->tam_y;
+	*status = p->status;
+	*speed = p->speed;
+	free (p);
+
+	return 1;
+}
--- spaceinvaders.c
+++ spaceinvaders.c
@@ -2,24 +2,63 @@
 #include <unistd.h>
 #include <ncurses.h>
 
+#include "lib_lista_space.h"
+
+typedef struct Game {
+	int score;
+	int level;
+	int maxRows;
+	int maxCols;
+} t_game;
+
+typedef struct Elements {
+	t_lista aliens;
+	t_lista structures;
+	t_lista shots;
+	t_lista bombs;
+} t_elements;
+
+
+int  setupInput         (t_game *game);
 int  validateWindowSize (int *rows, int *cols);
-void setupInput         ();
+void startGame          (t_game *game, t_elements *elements);
+void inicializeLists    (t_elements *elements);
+void addAliens          (t_elements *elements);
+void addStructures      (t_elements *elements);
+void playGame           (t_game *game, t_elements *elements);
 void printScreen        (int rows, int cols);
 
 int main ()
 {
-	initscr ();
+	t_game game;
+	t_elements elements;
 
-	int rows, cols;
-	if (validateWindowSize (&rows, &cols))
+	if (!setupInput (&game))
 		return 1;
-	
-	/*setupInput ();*/
 
-	printScreen ();
+	startGame (&game, &elements);
+
+	playGame (&game, &elements);
+
+	printScreen (game.maxRows, game.maxCols);
 
 	endwin();
 	return 0;
+}
+
+int setupInput (t_game *game)
+{
+	initscr();
+	cbreak();               /* desabilita o buffer de entrada */
+	noecho();               /* não mostra os caracteres digitados */
+	nodelay(stdscr, TRUE);  /* faz com que getch não aguarde a digitação */
+	keypad(stdscr, TRUE);   /* permite a leitura das setas */
+	curs_set(FALSE);        /* não mostra o cursor na tela */
+
+	if (!validateWindowSize (&game->maxRows, &game->maxCols))
+		return 0;
+
+	return 1;
 }
 
 int validateWindowSize (int *rows, int *cols)
@@ -28,32 +67,48 @@ int validateWindowSize (int *rows, int *cols)
 
 	if (*rows < 38 || *cols < 100)
 	{
-		printf ("The terminal window is too small.\n");
 		endwin();
+		printf ("Window is too small.\nMinimum size: 38x100.\nCurrent size: %dx%d\n",*rows,*cols);
 		return 0;
 	}
 
 	return 1;	
 }
 
-void setupInput ()
+void startGame(t_game *game, t_elements *elements)
 {
-	cbreak();               /* desabilita o buffer de entrada */
-	noecho();               /* não mostra os caracteres digitados */
-	nodelay(stdscr, TRUE);  /* faz com que getch não aguarde a digitação */
-	keypad(stdscr, TRUE);   /* permite a leitura das setas */
-	curs_set(FALSE);        /* não mostra o cursor na tela */
+	game->score = 0;
+	game->level = 1;
+
+	inicializeLists (elements);
+
+	addAliens (elements);
+	addStructures (elements);
 }
+
+void inicializeLists (t_elements *elements)
+{
+	inicializa_lista (&elements->aliens);
+	inicializa_lista (&elements->structures);
+	inicializa_lista (&elements->shots);
+	inicializa_lista (&elements->bombs);
+}
+
+void addAliens (t_elements *elements){}
+void addStructures (t_elements *elements){}
+
+void playGame(t_game *game, t_elements *elements){}
 
 void printScreen (int rows, int cols)
 {
-	clear();
+	erase();
 
 	int i, j;
 	for ( i=0; i < rows; i++ )
 		for ( j=0; j < cols; j++ )
 		{
-			move(i,j);
-			addch (m.[i][j]);
+			mvaddch(i,j,'.');
 		}
+	refresh();
+	sleep(2);
 }
